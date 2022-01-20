ad202ac49655e57685904e5b7b062ac2ebefa450
Robert Godlewski <robtechg5@gmail.com>
2022-01-19 17:27:51 -0800

    Added in dojo, say, and repreat get responses.  Repeat prints the phrase in the terminal.

---
+++ README.md
@@ -0,0 +1,4 @@
+http://127.0.0.1:5000/ is the main link
+
+To start the environment do $ pipenv shell
+To end the environment do (folder_name) .... $ exit
--- server.py
+++ server.py
@@ -7,5 +7,21 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world(): return 'Hello World!'
 
+@app.route('/dojo')
+def dojo(): return 'Dojo!'
+
+@app.route('/say/<phrase>')
+def say_phrase(phrase): return f"Hi {str(phrase)}!"
+
+@app.route('/repeat/<times>/<phrase>')
+def repeat(times, phrase):
+    num_times = int(times)
+    str_phrase = str(phrase)
+    i = 0
+    while i < num_times:
+        print(str_phrase)
+        i += 1
+    return str_phrase
+
 
 if __name__=="__main__": app.run(debug=True)
